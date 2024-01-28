07047af0669a1e6bac2d0e4a3ec476bdc3ee47b2
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-01-28 12:42:46 +0530

    Add theme toggle

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -131,7 +131,7 @@ const origin = searchParams.get('origin')
                                 <span className='w-full border-t' />
                             </div>
                             <div className="relative flex justify-center text-xs uppercase">
-                                <span className="bg-background px-2 text-muted-foreground">
+                                <span className="bg-white px-2 text-muted-foreground">
                                     or
                                 </span>
                             </div>
